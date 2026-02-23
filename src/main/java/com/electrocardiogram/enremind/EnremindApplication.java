2fd1e7748dc83fb1a1c5439a5c531c85cc270308
dragon <GLDZPang@outlook.com>
2026-02-23 22:10:23 +0800

    feat: init rep

---
+++ src/main/java/com/electrocardiogram/enremind/EnremindApplication.java
@@ -0,0 +1,13 @@
+package com.electrocardiogram.enremind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnremindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnremindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/electrocardiogram/enremind/EnremindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electrocardiogram.enremind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnremindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
