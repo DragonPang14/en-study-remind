35492b276799bc70f65d50bbd776633e708905c2
dragon <GLDZPang@outlook.com>
2026-02-28 18:35:42 +0800

    feat: add scheduler

--- src/test/java/com/electrocardiogram/esr/EnStudyRemindApplicationTests.java
+++ src/test/java/com/electrocardiogram/esr/EnStudyRemindApplicationTests.java
@@ -2,8 +2,10 @@ package com.electrocardiogram.esr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootTest
+@EnableScheduling
 class EnStudyRemindApplicationTests {
 
     @Test
